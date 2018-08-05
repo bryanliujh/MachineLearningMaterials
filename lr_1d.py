cefe4b15e9b9bef9544bad79c9d777240c84e18f
bryanliujh <bbx54ever@hotmail.com>
2018-08-05 20:18:36 +0800

    first commit

---
+++ lr_1d.py
@@ -0,0 +1,24 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+X = []
+Y = []
+for line in open('data_1d.csv'):
+    x, y = line.split(',')
+    X.append(float(x))
+    Y.append(float(y))
+X = np.array(X)
+Y = np.array(Y)
+
+# apply eqn to calc a and b
+denominator = X.dot(X) - X.mean() * X.sum()
+a = (X.dot(Y) - Y.mean() * X.sum()) / denominator
+b = (Y.mean() * X.dot(X) - X.mean() * X.dot(Y)) / denominator
+
+Yhat = a * X + b
+
+plt.scatter(X, Y)
+plt.plot(X, Yhat)
+plt.show()
+
+
