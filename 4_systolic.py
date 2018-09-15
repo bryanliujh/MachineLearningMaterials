f78f1ff60b8cb18770147b386b5343bbc98d5200
bryanliujh <bbx54ever@hotmail.com>
2018-09-15 17:25:30 +0800

    multiple linear regression

---
+++ 4_systolic.py
@@ -0,0 +1,8 @@
+# The data (X1, X2, X3) are for each patient.
+# X1 = systolic blood pressure
+# X2 = age in years
+# X3 = weight in pounds
+
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
